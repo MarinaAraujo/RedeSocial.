9c15eeea82d999aa5b6d38376324e1a96a0b261a
MarinaAraujo <marinamarttinsmap@gmail.com>
2017-11-08 08:31:07 -0200

    Update usuario.py

--- usuario.py
+++ usuario.py
@@ -1,28 +1,59 @@
 from App import *
 import sqlite3
 
+class Usuario():
+    def __init__(self,codigo, nome, email, senha, idade, telefone, profissao):
+        self.codigo = codigo
+        self.nome = nome
+        self.email = email
+        self.senha = senha
+        self.idade = idade
+        self.telefone = telefone
+        self.profissao = profissao
 
-conn = sqlite3.connect("novacon.db")
+    def inserir(self):
+            conn = sqlite3.connect("novacon.db")
+            cursor= conn.cursor()
+            cursor.execute("""
+            INSERT INTO tb.usuario (nome, email, senha, idade, telefone, profissao)
+            VALUES (self.nome,self.email,self.senha,self.idade,self.telefone,self.profissao)
+            """)
+            conn.commit()
+            conn.close()
 
-cursor= conn.cursor()
+
+    def listar(self):
+        usuarios = []
+            conn = sqlite3.connect("novacon.db")
+            cursor= conn.cursor()
+            cursor.execute("""SELECT * FROM tb.usuario """, (tb.usuario))
+
+            for linha in cursor.fetchall:
+                nome = linha[1]
+                email=linha[2]
+                senha=linha[3]
+                idade=linha[4]
+                telefone=linha[5]
+                profissao=linha[6]
+                usuario = Usuario(nome, email, senha, profissao, sexo, data_nasc)
+                usuarios.append(usuario)
+        conn.close()
+        return usuarios
+
+    def deletar(self, id):
+        pass
+
+    def atualizar(self, nome, email, senha, profissao, sexo, data_nasc):
+        pass
+
+
 
 cursor.execute("""
-INSERT INTO tb.usuario ( codigo, nome, email, senha, idade, telefone, profissao)
-VALUES ("3333", "kleber", "fjdhnd@gmail.com", "12345", "18", "55544432", "professor")
-""")
-
-cursor.execute("""SELECT * FROM tb.usuario """, (tb.usuario))
-
-cursor.execute("""
-UPDATE tb.usuario
+''' UPDATE tb.usuario
 SET codigo=?
 WHERE id=?
 """, ())
 
 cursor.execute("""
 DELETE FROM tb.usuario
-WHERE id=?""", ())
-
-
-conn.commit()
-conn.close()
+WHERE id=?""", ()) '''
