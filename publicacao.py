9f5a5c97817aca2eff31c0d7e23d2bbc27690cef
MarinaAraujo <marinamarttinsmap@gmail.com>
2017-11-11 16:21:38 -0200

    Update publicacao.py

--- publicacao.py
+++ publicacao.py
@@ -1,27 +1,65 @@
-from App import *
+#from App import *
 import sqlite3
 
 
-conn = sqlite3.connect("novacon.db")
+class Publicacao:
+    def __init__(self,idPublicacao,idRemetente,horario,texto,nToploose,nXonei,nHu3,nExasperado,nSad):
+        self.idPublicacao = idPublicacao
+        self.idRemetente = idRemetente
+        self.horario = horario
+        self.texto = texto
+        self.nToploose = nToploose
+        self.nXonei = nXonei
+        self.nHu3 = nHu3
+        self.nExasperado = nExasperado
+        self.nSad = nSad
 
-cursor= conn.cursor()
+    def inserir ():
+        conn = sqlite3.connect("novacon.db")
+        cursor = conn.cursor()
+        cursor.execute("""
+            INSERT INTO tb.publicacao (idRemetente, horario, texto, nToploose,nXonei,nHu3,nExasperado, nSad)
+            VALUES (self.idRemetente,self.horario,self.texto,self.nToploose,self.nXonei,self.nHu3,self.nExasperado,self.nSad)
+            """)
+        conn.commit()
+        conn.close()
 
-cursor.execute("""
-INSERT INTO tb.publicacao (idRemetente,idPublicacao, horario, texto,publico, nToploose,nXonei,nHu3,nExasperado, nSad)
-VALUES ("54654", "5645", "20:00", "ggbfbfhssghthdf", "somente eu", "90", "5", "65", "35", "3")
-""")
+    def listar():
+        publicacoes=[]
+        conn = sqlite3.connect("novacon.db")
+        cursor = conn.cursor()
+        cursor.execute("""SELECT * FROM tb.publicacao """, (tb.publicacao))
 
-cursor.execute("""SELECT * FROM tb.publicacao """, (tb.publicacao))
+        for linha in cursor.fetchall:
+            idRemetente = linha[1]
+            horario = linha[2]
+            texto = linha[3]
+            nToploose = linha[4]
+            nXonei = linha[5]
+            nHu3 = linha[6]
+            nExasperado = linha[7]
+            nSad = linha[8]
+            Publicacao = Publicacao(idRemetente, horario, texto, nToploose,nXonei,nHu3,nExasperado, nSad)
+            publicacoes.append(Publicacao)
+        conn.close()
+        return publicacoes
 
-cursor.execute("""
-UPDATE tb.publicacao
-SET horario=?
-WHERE h=?
-""", ())
+    def deletar (self, codigo):
+        novo_id = 4
+        conn = sqlite3.connect("novacon.db")
+        cursor = conn.cursor()
+        cursor.execute("""
+            DELETE FROM tb.publicacao
+            WHERE idPublicacao=?""", (novo_id,))
+        conn.commit()
+        conn.close()
 
-cursor.execute("""
-DELETE FROM tb.publicacao
-WHERE publico=?""", ())
+    def atualizar (self,idRemetente, horario, texto, nToploose,nXonei,nHu3,nExasperado, nSad):
+        conn = sqlite3.connect("novacon.db")
+        cursor = conn.cursor()
+        cursor.execute("""UPDATE tb.publicacao
+        SET * VALUES(?,?,?,?,?,?,?)
+        WHERE idPublicacao = ?""",())
 
-conn.commit()
-conn.close()
+        conn.commit()
+        conn.close()
