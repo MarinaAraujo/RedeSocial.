5ed85dfde3b783ca4ed2d4b1fadc1952bf371190
MarinaAraujo <marinamarttinsmap@gmail.com>
2017-12-12 23:32:17 -0200

    Update Menssagem.py

--- Model/Menssagem.py
+++ Model/Menssagem.py
@@ -8,43 +8,4 @@ class Mensagem ():
         self.coments = coments
 
 
-    def inserir():
-        conn = sqlite3.connect("novacon.db")
-        cursor= conn.cursor()
-        cursor.execute("""
-            INSERT INTO tb.mensagem (idRemetente, coments)
-            VALUES (self.idRemetente, self.coments)
-            """)
-
-    def listar():
-        mensagens = []
-        conn = sqlite3.connect("novacon.db")
-        cursor = conn.cursor()
-        cursor.execute("""SELECT * FROM tb.mensagem """, (tb.mensagem))
-
-        for linha in cursor.fetchall:
-            idRemetente = linha[1]
-            coments = linha[2]
-            Mensagem = Mensagem(idRemetente, coments)
-            mensagens.append(Mensagem)
-        conn.close()
-        return mensagens
-
-    def deletar(self, id):
-        novo_id = 3
-        conn = sqlite3.connect("novacon.db")
-        cursor = conn.cursor()
-        cursor.execute("""
-                DELETE FROM tb.mensagem
-                WHERE id=?""", (novo_id,))
-        conn.commit()
-        conn.close()
-
-    def atualizar(self, idRemetente, coments):
-        conn = sqlite3.connect("novacon.db")
-        cursor = conn.cursor()
-        cursor.execute("""UPDATE tb.mensagem
-            SET * VALUES(?,?,?,?,?,?,?)
-            WHERE id = ?""", ())
-        conn.commit()
-        conn.close()
+    
